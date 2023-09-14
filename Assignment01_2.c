c74b3466bbed460781c7b39752e2c4eb0fba65c7
E1-80545-Sahil <sahilbadgujar071@gmail.com>
2023-09-14 18:56:44 +0530

    My first commit

---
+++ Assignment01_2.c
@@ -0,0 +1,14 @@
+	
+	#include<stdio.h>
+	int main(void)
+	{	
+		int a;
+
+		printf("Enter a number: ");
+		scanf("%d",&a);
+
+		printf("Given Number in Char: %c\n",a);	
+		printf("Given Number in octal: %o\n",a);
+		printf("Given Number in Hexadecimal: %x \n",a);
+	return 0;
+	}
