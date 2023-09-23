f0471f2b7257477fe0eca3076698fe4c161ba19f
E1-80545-Sahil <sahilbadgujar071@gmail.com>
2023-09-24 00:39:15 +0530

    Assignment-1 Q.11, Q.12

---
+++ Assign01_11.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+
+int main(void)
+{
+	printf("Ranges of Data Types: \n");
+	printf("Data Type \t\t Size \t Range \n");
+	printf("signed char \t\t %d \t %d to %d \n", sizeof(char), SCHAR_MIN, SCHAR_MAX);
+	printf("unsigned char \t\t %d \t 0 to %d \n", sizeof(char), UCHAR_MAX);
+	printf("short int  \t\t %d \t %d to %d \n", sizeof(short int), SHRT_MIN, SHRT_MAX);
+	printf("unsigned short int \t %d \t 0 to %d \n", sizeof(unsigned short), USHRT_MAX);
+	printf("int \t\t\t %d \t %d to %d \n", sizeof(int), INT_MIN, INT_MAX);
+	printf("unsigned int \t\t %d \t 0 to %u \n", sizeof(unsigned int), UINT_MAX);
+	printf("long int \t\t %d \t %ld to %ld \n",sizeof(long int), LONG_MIN, LONG_MAX);	
+	printf("unsigned long int \t %d \t 0 to %lu \n", sizeof(unsigned long int), ULONG_MAX);
+	return 0;
+}
